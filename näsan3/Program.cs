d7447a813efdaeecc058c4b8b105427c04ec38fa
Kamil Kanngo <kamil.kanngo@elev.ga.ntig.se>
2024-11-29 14:34:49 +0100

    Klar

---
+++ näsan3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hello32();
+    }
+
+    static void Hello32()
+    {
+        int count = 0;
+        while (count < 32)
+        {
+            Console.WriteLine("Hello, World");
+            count++;
+        }
+    }
+}
